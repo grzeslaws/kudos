5ab763665be66e38ebd21c6b5abfc3abdf9c0314
grzeslaws <grzesupel@gmail.com>
2019-01-31 14:35:19 +0100

    Small fixes

--- frontend/src/components/layout/LayoutComponent.tsx
+++ frontend/src/components/layout/LayoutComponent.tsx
@@ -1,31 +1,38 @@
 import * as React from "react";
 import { IContext } from "../ProviderContextComponent";
 import wrapperComponent from "../WrapperComponent";
-import { Navbar, WrapperFull, Photos, WrapperColumns, Column } from 'src/components/layout/LayoutStyled';
-import { ButtonPure } from 'src/theme/objects/Buttons';
-import { AddKudosComponent } from '../add-kudos/AddKudosComponent';
-import { ProfileComponent } from '../profile/ProfileComponent';
-import { DescriptionComponent } from '../description/DescriptionComponent';
-import { TopPicksComponent } from '../top-picks/TopPicksComponent';
-import { KudosListComponent } from '../kudos-list/KudosListComponent';
-import { ProfileImage } from 'src/theme/objects/Images';
-import { WrapperMain } from 'src/theme/objects/Layout';
+import { Navbar, WrapperFull, Photos, WrapperColumns, Column } from "src/components/layout/LayoutStyled";
+import { ButtonPure } from "src/theme/objects/Buttons";
+import { AddKudosComponent } from "../add-kudos/AddKudosComponent";
+import { ProfileComponent } from "../profile/ProfileComponent";
+import { DescriptionComponent } from "../description/DescriptionComponent";
+import { TopPicksComponent } from "../top-picks/TopPicksComponent";
+import { KudosListComponent } from "../kudos-list/KudosListComponent";
+import { ProfileImage } from "src/theme/objects/Images";
+import { WrapperMain } from "src/theme/objects/Layout";
 
 export interface Props {
     context?: IContext;
 }
 
-class Layout extends React.Component<Props> {
+interface State {
+    showProfile: boolean;
+}
+
+class Layout extends React.Component<Props, State> {
+    public readonly state = {
+        showProfile: false,
+    };
     public render() {
         return (
             <WrapperMain>
                 <Navbar>
                     <ButtonPure href="#">Give kudos</ButtonPure>
-                    <ProfileImage small={true} path={this.props.context!.profile!.image} />
+                    <ProfileImage onMouseOver={this.showProfile} small={true} path={this.props.context!.profile!.image} />
+                    <ProfileComponent show={this.state.showProfile} />
                 </Navbar>
                 <WrapperFull>
                     <AddKudosComponent />
-                    <ProfileComponent />
                 </WrapperFull>
                 <Photos />
                 <WrapperColumns>
@@ -42,6 +49,8 @@ class Layout extends React.Component<Props> {
             </WrapperMain>
         );
     }
+
+    private showProfile = () => this.setState({ showProfile: true });
 }
 
 export const LayoutComponent = wrapperComponent(Layout);
